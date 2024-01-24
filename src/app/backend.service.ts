fc8f3ec788afffcdf6d3b79119c3555caab55c42
Thomas Gainant <thomas.gainant@axelspringer.com>
2024-01-24 10:57:46 +0100

    added session id

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { env } from '../env';
+import { v4 as uuid } from 'uuid';
 import { ChatEntry } from '../models/chat-entry';
 import { BehaviorSubject } from 'rxjs';
 
@@ -9,18 +10,22 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class BackendService {
 
+  public sessionID:string = uuid();
+  //public sessionID:string = "ab09e5b7-5f01-45fe-8f3d-42bf25d1aef7";
+
   public entries:BehaviorSubject<ChatEntry[]> = new BehaviorSubject<ChatEntry[]>([]);
 
   constructor(private http: HttpClient) { }
 
   getEntries(){
-    this.http.get(env.getURL).subscribe((res:any) => {
-      this.entries.next(res.entries!);
+    this.http.get(env.getURL+"/"+this.sessionID).subscribe((res:any) => {
+      this.entries.next(res.prompts!);
     });
   }
 
   sendRequest(content:string){
-    this.http.post(env.sendURL, new ChatEntry(content, false)).subscribe(() => {
+    this.entries.getValue().push(new ChatEntry(content, true));
+    this.http.post(env.sendURL+"/"+this.sessionID, {"prompt": this.entries}).subscribe(() => {
       
     });
   }
--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-    "getURL": "https://europe-west3-aspringer-hackathon24ber-6081.cloudfunctions.net/google-hackathon-focus-group-get-entries/get",
-    "sendURL": "https://europe-west3-aspringer-hackathon24ber-6081.cloudfunctions.net/google-hackathon-focus-group-send-request/send",
+    "getURL": "https://europe-west3-aspringer-hackathon24ber-6081.cloudfunctions.net/google-hackathon-focus-group-pre-proxy/api/get",
+    "sendURL": "https://europe-west3-aspringer-hackathon24ber-6081.cloudfunctions.net/google-hackathon-focus-group-pre-proxy/api/send",
 
     "refreshInterval": 5000
 }
